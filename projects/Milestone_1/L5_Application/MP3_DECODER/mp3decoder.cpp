9da452894417f89e9e9f9a5c889ac996ae022546
chadwpalm <chadwpalm@gmail.com>
2019-04-27 12:49:38 -0700

    Initialized decoder, read status regs

--- projects/Milestone_1/L5_Application/MP3_DECODER/mp3decoder.cpp
+++ projects/Milestone_1/L5_Application/MP3_DECODER/mp3decoder.cpp
@@ -5,73 +5,90 @@
  *      Author: chadw
  */
 #include <MP3_DECODER/mp3decoder.h>
+#include <stdio.h>
 
-static mp3FilePlayer *myself;
-volatile bool feedBufferLock = false;
+GPIO cs_(P0_0);
+GPIO dreq_(P0_1);
+GPIO xdcs_(P0_29);
+GPIO reset_(P0_30);
 
-static void feeder(void) {
-  myself->feedBuffer();
+mp3Decoder::mp3Decoder()
+{
 }
 
-
-mp3FilePlayer::mp3FilePlayer(int8_t rst, int8_t cs, int8_t dcs, int8_t dreq, int8_t cardCS)
+bool mp3Decoder::begin(void)
 {
-    playingMusic = false;
-    _cardCS = cardCS;
-}
+    SPI.initialize(8, LabSpi::tSPI, 16);
+    cs_.setAsOutput();
+    cs_.setHigh();
+    xdcs_.setAsOutput();
+    xdcs_.setHigh();
+    dreq_.setAsInput();
+    reset_.setAsOutput();
+    reset_.setLow();
 
-bool mp3FilePlayer::begin(void)
-{
-    pinMode(_cardCS, OUTPUT);
-    digitalWrite(_cardCS, HIGH);
+    reset();
 
-    uint8_t v  = Adafruit_VS1053::begin();
-
-    //dumpRegs();
-    //Serial.print("Version = "); Serial.println(v);
+    uint8_t v  = (sciRead(VS1053_REG_STATUS) >> 4) & 0x0F;
+    printf("Version: %d\n", v);
+    printf("Mode = 0x%x\n", sciRead(VS1053_REG_MODE));
+    printf("Stat = 0x%x\n", sciRead(VS1053_REG_STATUS));
+    printf("ClkF = 0x%x\n", sciRead(VS1053_REG_CLOCKF));
+    printf("Vol. = 0x%x\n", sciRead(VS1053_REG_VOLUME));
     return (v == 4);
 }
 
-bool mp3FilePlayer::useInterrupt(uint8_t type)
+void mp3Decoder::reset(void)
 {
+    reset_.setLow();
+    delay_ms(100);
+    reset_.setHigh();
+    cs_.setHigh();
+    xdcs_.setHigh();
+    delay_ms(100);
+    sciWrite(VS1053_REG_MODE, VS1053_MODE_SM_SDINEW | VS1053_MODE_SM_RESET);
+    delay_ms(200);
+
+    sciWrite(VS1053_REG_CLOCKF, 0x6000);
+    setVolume(40, 40);
 }
 
-void mp3FilePlayer::feedBuffer(void)
+uint16_t mp3Decoder::sciRead(uint8_t addr)
 {
+    uint16_t data;
+
+    cs_.setLow();
+    SPI.transfer(VS1053_SCI_READ);
+    SPI.transfer(addr);
+
+    //delay_us(10);
+    data = SPI.transfer(0x00);
+    data <<= 8;
+    data |= SPI.transfer(0x00);
+    cs_.setHigh();
+    return data;
 }
 
-bool mp3FilePlayer::isMP3File(const char* fileName)
+void mp3Decoder::sciWrite(uint8_t addr, uint16_t data)
 {
+    cs_.setLow();
+    SPI.transfer(VS1053_SCI_WRITE);
+    SPI.transfer(addr);
+    SPI.transfer(data >> 8);
+    SPI.transfer(data & 0xFF);
+    cs_.setHigh();
 }
 
-unsigned long mp3FilePlayer::mp3_ID3Jumper(File mp3)
+void mp3Decoder::setVolume(uint8_t left, uint8_t right)
 {
+    uint16_t vol;
+    vol = left;
+    vol <<= 8;
+    vol |= right;
+
+    sciWrite(VS1053_REG_VOLUME, vol);
 }
 
-bool mp3FilePlayer::startPlayingFile(const char* trackname)
-{
-}
-
-bool mp3FilePlayer::playFullFile(const char* trackname)
-{
-}
-
-void mp3FilePlayer::stopPlaying(void)
-{
-}
-
-bool mp3FilePlayer::paused(void)
-{
-}
-
-bool mp3FilePlayer::stopped(void)
-{
-}
-
-void mp3FilePlayer::pausePlaying(bool pause)
-{
-}
-
-void mp3FilePlayer::feedBuffer_noLock(void)
+void mp3Decoder::sineTest(uint8_t n, uint16_t ms)
 {
 }
--- projects/Milestone_1/L5_Application/MP3_DECODER/mp3decoder.h
+++ projects/Milestone_1/L5_Application/MP3_DECODER/mp3decoder.h
@@ -11,12 +11,9 @@
 #include <SPI/LabSpi.hpp>
 #include <ff.h>
 #include <io.hpp>
+#include <gpio.hpp>
+#include <utilities.h>
 
-typedef volatile uint32_t RwReg;
-typedef uint32_t PortMask;
-typedef volatile RwReg PortReg;
-
-#define VS1053_FILEPLAYER_TIMER0_INT 255 // allows useInterrupt to accept pins 0 to 254
 #define VS1053_FILEPLAYER_PIN_INT 5
 
 #define VS1053_SCI_READ 0x03
@@ -34,12 +31,6 @@ typedef volatile RwReg PortReg;
 #define VS1053_REG_HDAT1 0x09
 #define VS1053_REG_VOLUME 0x0B
 
-#define VS1053_GPIO_DDR 0xC017
-#define VS1053_GPIO_IDATA 0xC018
-#define VS1053_GPIO_ODATA 0xC019
-
-#define VS1053_INT_ENABLE  0xC01A
-
 #define VS1053_MODE_SM_DIFF 0x0001
 #define VS1053_MODE_SM_LAYER12 0x0002
 #define VS1053_MODE_SM_RESET 0x0004
@@ -52,78 +43,49 @@ typedef volatile RwReg PortReg;
 #define VS1053_MODE_SM_LINE1 0x4000
 #define VS1053_MODE_SM_CLKRANGE 0x8000
 
-
-#define VS1053_SCI_AIADDR 0x0A
-#define VS1053_SCI_AICTRL0 0x0C
-#define VS1053_SCI_AICTRL1 0x0D
-#define VS1053_SCI_AICTRL2 0x0E
-#define VS1053_SCI_AICTRL3 0x0F
-
 #define VS1053_DATABUFFERLEN 512
 
 class mp3Decoder {
 public:
     //Constructor
-    mp3Decoder(int8_t rst, int8_t cs, int8_t dcs, int8_t dreq);
+    mp3Decoder();
 
-    uint8_t begin(void);
-      void reset(void);
-      void softReset(void);
-      uint16_t sciRead(uint8_t addr);
-      void sciWrite(uint8_t addr, uint16_t data);
-      void sineTest(uint8_t n, uint16_t ms);
-      void spiwrite(uint8_t d);
-      void spiwrite(uint8_t *c, uint16_t num);
-      uint8_t spiread(void);
+    bool begin(void);
+    void reset(void);
+    void setVolume(uint8_t left, uint8_t right);
 
-      uint16_t decodeTime(void);
-      void setVolume(uint8_t left, uint8_t right);
-      void dumpRegs(void);
+private:
+    uint16_t sciRead(uint8_t addr);
+    void sciWrite(uint8_t addr, uint16_t data);
+    void sineTest(uint8_t n, uint16_t ms);
 
-      void playData(uint8_t *buffer, uint8_t buffsiz);
-      bool readyForData(void);
-      void applyPatch(const uint16_t *patch, uint16_t patchsize);
-      uint16_t loadPlugin(char *fn);
+    LabSpi SPI;
 
-      void GPIO_digitalWrite(uint8_t i, uint8_t val);
-      void GPIO_digitalWrite(uint8_t i);
-      uint16_t GPIO_digitalRead(void);
-      bool GPIO_digitalRead(uint8_t i);
-      void GPIO_pinMode(uint8_t i, uint8_t dir);
-
-      bool prepareRecordOgg(char *plugin);
-      void startRecordOgg(bool mic);
-      void stopRecordOgg(void);
-      uint16_t recordedWordsWaiting(void);
-      uint16_t recordedReadWord(void);
-
-      uint8_t mp3buffer[VS1053_DATABUFFERLEN];
+    uint8_t mp3buffer[VS1053_DATABUFFERLEN];
 };
 
-class mp3FilePlayer : public mp3Decoder {
- public:
-
-  mp3FilePlayer (int8_t rst, int8_t cs, int8_t dcs, int8_t dreq,
-                  int8_t cardCS);
-
-  bool begin(void);
-  bool useInterrupt(uint8_t type);
-  //File currentTrack;
-  volatile bool playingMusic;
-  void feedBuffer(void);
-  static bool isMP3File(const char* fileName);
-  unsigned long mp3_ID3Jumper(File mp3);
-  bool startPlayingFile(const char *trackname);
-  bool playFullFile(const char *trackname);
-  void stopPlaying(void);
-  bool paused(void);
-  bool stopped(void);
-  void pausePlaying(bool pause);
-
- private:
-  void feedBuffer_noLock(void);
-
-  //uint8_t _cardCS;
-};
+//class mp3FilePlayer : public mp3Decoder {
+// public:
+//
+//  mp3FilePlayer (int8_t rst, int8_t cs, int8_t dcs, int8_t dreq);
+//
+//  bool begin(void);
+//  bool useInterrupt(uint8_t type);
+//  //File currentTrack;
+//  volatile bool playingMusic;
+//  void feedBuffer(void);
+//  static bool isMP3File(const char* fileName);
+//  bool startPlayingFile(const char *trackname);
+//  bool playFullFile(const char *trackname);
+//  void stopPlaying(void);
+//  bool paused(void);
+//  bool stopped(void);
+//  void pausePlaying(bool pause);
+//
+// private:
+//  void feedBuffer_noLock(void);
+//
+//  //uint8_t _cardCS;
+//};
 
 #endif /* MP3DECODER_H_ */
